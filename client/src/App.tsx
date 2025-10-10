40286c9c94de68f2c1c1f921fe5e475f1033628f
Joshua Jarvis <joshuajarvis0711@gmail.com>
2025-10-10 11:34:47 -0500

    Added react app scaffold.

---
+++ client/src/App.tsx
@@ -0,0 +1,8 @@
+import "./App.css";
+import BankCard from "./bank-card/BankCard";
+
+function App() {
+  return <BankCard></BankCard>;
+}
+
+export default App;
---
+++ client/src/bank-card/BankCard.tsx
@@ -0,0 +1,36 @@
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import * as React from "react";
+
+const card = (
+  <React.Fragment>
+    <CardContent>
+      <Typography variant="h5" component="div">
+        Bank Account Balance
+      </Typography>
+      <Typography sx={{ color: "text.secondary", mb: 1.5 }}>
+        adjective
+      </Typography>
+      <Typography variant="body2">
+        well meaning and kindly.
+        <br />
+        {'"a benevolent smile"'}
+      </Typography>
+    </CardContent>
+    <CardActions>
+      <Button size="small">Learn More</Button>
+    </CardActions>
+  </React.Fragment>
+);
+
+export default function OutlinedCard() {
+  return (
+    <Box sx={{ minWidth: 275 }}>
+      <Card variant="outlined">{card}</Card>
+    </Box>
+  );
+}
